182ab96b3942a498b55f23abf786a1c19bb0c9da
Sushant <stha2022sushant@gmail.com>
2024-11-30 23:06:07 +0545

    just to inform this is bad coding practice

--- hello.js
+++ hello.js
@@ -1,3 +1,5 @@
 console.log('Hello Git !');
 console.log('Hello there, This is GitTest');
 console.log('this line of information was edited by sushant');
+console.log('This is bad coding practice');
+
